317f384358477e2d5383355201b9d243f74e811e
ARRAKIS-DEV <elouazzani.amine@gmail.com>
2016-12-17 21:31:49 +0100

    fix avi convector

--- ArrakisServices/controllers/ActionsControllers.php
+++ ArrakisServices/controllers/ActionsControllers.php
@@ -23,7 +23,7 @@ class ActionsControllers //extends MyController
 		$extension=strtoupper(strrev(explode('.',strrev(strrev(explode('/',strrev($PathOrigin))[0])))[0]));
 		 $NameVideos=str_replace('.'.strtolower($extension),$vide,strrev(explode('/',strrev($PathOrigin))[0]));
 		 $URL_COVERT_VIDEOS=$SRV_CONVERT.$NameVideos.'.mp4';
-		
+		 return $URL_COVERT_VIDEOS;
 		 if ($extension=='AVI')
 		 {
 		 $commandeShell='sudo avconv -i '.$PathOrigin.' -c:v libx264 -c:a copy '.str_replace('http://192.168.0.44','/var/www/html',$URL_COVERT_VIDEOS);
