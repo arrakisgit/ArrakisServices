c2c0cdc552a723ae9a90f85413939c00483fb85b
ARRAKIS-DEV <elouazzani.amine@gmail.com>
2016-12-17 05:49:07 +0100

    test service

--- ArrakisServices/index.php
+++ ArrakisServices/index.php
@@ -80,9 +80,9 @@ class Request
 			switch($content_type)
 			{
 				case "application/json":
-					$this->parameters =$body;
-					$body_params = json_decode($body);
-					//$this->parameters =$body_params;
+					//$this->parameters =$body;
+					$body_params = json_decode(utf8_encode($body));
+					$this->parameters =$body_params;
 					if($body_params)
 					{
 						foreach($body_params as $param_name => $param_value)
