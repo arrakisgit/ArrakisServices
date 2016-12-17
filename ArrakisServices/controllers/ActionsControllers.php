f2b4920e74fdea0c3dec62b70c4f09c0913ab009
ARRAKIS-DEV <elouazzani.amine@gmail.com>
2016-12-17 21:19:35 +0100

    test avi convector

--- ArrakisServices/controllers/ActionsControllers.php
+++ ArrakisServices/controllers/ActionsControllers.php
@@ -15,11 +15,34 @@ class ActionsControllers //extends MyController
 	public function postAction($param)
 	{
 		//return $param;
-		$urlPath=$param['urlPath'];
+		$PathOrigin=$param['urlPath'];
 		//return $urlPath;
 		//$pConverted=new Convertor($urlPath, $this->ServerPath);
-		$data=Convectors($urlPath);
-		return $data;
+		$vide='';
+		$SRV_CONVERT='http://192.168.0.44/ArrakisWeb/ArrakisVideos/';
+		$extension=strtoupper(strrev(explode('.',strrev(strrev(explode('/',strrev($PathOrigin))[0])))[0]));
+		 $NameVideos=str_replace('.'.strtolower($extension),$vide,strrev(explode('/',strrev($PathOrigin))[0]));
+		 $URL_COVERT_VIDEOS=$SRV_CONVERT.$NameVideos.'.mp4';
+		
+		 if ($extension=='AVI')
+		 {
+		 $commandeShell='sudo avconv -i '.$PathOrigin.' -c:v libx264 -c:a copy '.str_replace('http://192.168.0.44','/var/www/html',$URL_COVERT_VIDEOS);
+		 $result=ExcuteShell($commandeShell);
+		
+		 }
+		 elseif ($extension=="MKV")
+		 {
+		 $commandeShell='sudo ffmpeg -i '.$urlPath.' -vcodec copy -acodec copy '.$URL_COVERT_VIDEOS;
+		 $result=ExcuteShell($commandeShell);
+		
+		 }
+		 else
+		 {
+		 	
+		 }
+		 //$PathConvertedFile=$URL_COVERT_VIDEOS;
+		return $URL_COVERT_VIDEOS;
+		//return $data;
 	}
 	
 	public function Convectors($PathOrigine)
