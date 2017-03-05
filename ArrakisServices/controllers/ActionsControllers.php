8cc4cc938aa6edffd1160c955be374373f78cf9e
ARRAKIS-DEV <elouazzani.amine@gmail.com>
2017-03-05 11:55:44 +0100

    test api rest

--- ArrakisServices/controllers/ActionsControllers.php
+++ ArrakisServices/controllers/ActionsControllers.php
@@ -16,14 +16,14 @@ class ActionsControllers //extends MyController
 	{
 		//return $param;
 		$PathOrigin=$param['urlPath'];
-		//return $urlPath;
+		return $urlPath;
 		//$pConverted=new Convertor($urlPath, $this->ServerPath);
 		$vide='';
 		$SRV_CONVERT='http://192.168.0.44/ArrakisWeb/ArrakisVideos/';
 		$extension=strtoupper(strrev(explode('.',strrev(strrev(explode('/',strrev($PathOrigin))[0])))[0]));
 		 $NameVideos=str_replace('.'.strtolower($extension),$vide,strrev(explode('/',strrev($PathOrigin))[0]));
 		 $URL_COVERT_VIDEOS=$SRV_CONVERT.$NameVideos.'.mp4';
-		 return $extension;//$URL_COVERT_VIDEOS;
+		 //return $extension;//$URL_COVERT_VIDEOS;
 		 if ($extension=='AVI')
 		 {
 		 $commandeShell='sudo avconv -i '.$PathOrigin.' -c:v libx264 -c:a copy '.str_replace('http://192.168.0.44','/var/www/html',$URL_COVERT_VIDEOS);
