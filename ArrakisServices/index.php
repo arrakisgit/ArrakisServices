95528a5f7e3e53ffa150b0d9864735f4bb2b9dbf
ARRAKIS-DEV <elouazzani.amine@gmail.com>
2017-04-16 16:08:32 +0200

    test

--- ArrakisServices/index.php
+++ ArrakisServices/index.php
@@ -41,7 +41,7 @@ class Request
 		$this->url_elements = explode('/', $_SERVER['PATH_INFO']);
 		$uploaddir = realpath('./') . '/';
 		$uploadfile = $uploaddir . basename($_FILES['file_contents']['name']);
-		//return 'rrrrr';//$uploadfile;//'construct()';
+		return 'rrrrr';//$uploadfile;//'construct()';
 		if (move_uploaded_file($_FILES['file_contents']['tmp_name'], $uploadfile)) 
 		{
 			// route the request to the right place
