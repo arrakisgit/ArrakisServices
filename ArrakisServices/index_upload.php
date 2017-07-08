c9ebfcc190f68cc66c665b339f656c6b2768185e
ARRAKIS-DEV <elouazzani.amine@gmail.com>
2017-07-08 22:14:03 +0200

    test upload

--- ArrakisServices/index_upload.php
+++ ArrakisServices/index_upload.php
@@ -1,8 +1,8 @@
 <?php
-echo "<form action='http://192.168.0.23/ArrakisServices/ArrakisServices/controllers/SendMediaFiles.php' method='POST' enctype='multipart/form-data'>";;
-echo "<p>Images:";
-echo "<input type='file' name='userfile' />";
-echo "<input type='submit' value='Send' />";
+echo "<form action='http://192.168.0.23/ArrakisServices/ArrakisServices/controllers/SendMediaFiles.php' method='POST' enctype='multipart/form-data'>";
+echo "<p>Files:<br/>";
+echo "<input type='file' name='userfile' /><br/>";
+echo "<input type='submit' value='Send' /><br/>";
 echo "</p>";
 echo "</form>";
 ?>
